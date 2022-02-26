49bd8f2d59eb402c70b5cfce1f5e37fda82c939e
ariuka11 <ariun1118@gmail.com>
2022-02-26 12:58:15 -0500

    about page is setup

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,21 @@
+import photo from "../images/austin-wade-X6Uj51n5CE8-unsplash.jpg";
 const About = () => {
-  return <div className="aboutContainer">About</div>;
+  return (
+    <div className="aboutContainer">
+      <section>
+        <h2>About Me</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero
+          exercitationem illum cupiditate eaque, voluptatem laborum ullam quia
+          culpa adipisci quod doloribus minima excepturi aperiam nemo magnam
+          sapiente dignissimos doloremque dolores.
+        </p>
+      </section>
+      <section className="photo">
+        <img src={photo} alt="" />
+      </section>
+    </div>
+  );
 };
 
 export default About;
