22d68415811e4264becd9bc24d2b4a918afeb4a0
ariuka11 <ariun1118@gmail.com>
2022-02-26 15:56:13 -0500

    finished skills section

---
+++ src/components/Skills.tsx
@@ -0,0 +1,43 @@
+import { Icon } from "@iconify/react";
+import { motion } from "framer-motion";
+const Skills = () => {
+  return (
+    <div className="skillsContainer">
+      <section>
+        <h2>Skills</h2>
+      </section>
+      <motion.section className="gridSkill">
+        <Icon
+          icon="logos:javascript"
+          width={"100%"}
+          className="logo tall-wide"
+        />
+        <span className="text"> I know this very well</span>
+        <Icon icon="logos:react" width={"100%"} className="logo tall-wide" />
+        <Icon icon="logos:html-5" width={"100%"} className="logo" />
+        <Icon icon="logos:css-3" width={"100%"} className="logo" />
+        <Icon icon="logos:nodejs" width={"100%"} className="logo tall-wide" />
+        <Icon
+          icon="logos:mongodb"
+          color="black"
+          width={"100%"}
+          className="logo"
+        />
+        <Icon icon="logos:redux" width={"100%"} className="logo" />
+        <Icon icon="logos:typescript-icon" width={"100%"} className="logo" />
+        <Icon icon="logos:tailwindcss-icon" width={"100%"} className="logo" />
+        <Icon icon="logos:figma" width={"100%"} className="logo wide" />
+        <Icon icon="logos:nextjs-icon" width={"100%"} className="logo" />
+        <Icon icon="logos:express" width={"100%"} className="logo" />
+        <Icon icon="logos:git" width={"100%"} className="logo" />
+        <Icon icon="logos:vitejs" width={"100%"} className="logo" />
+        <Icon icon="logos:svelte-icon" width={"100%"} className="logo" />
+        <Icon icon="logos:python" width={"100%"} className="logo wide" />
+        <Icon icon="simple-icons:threedotjs" width={"100%"} className="logo" />
+        <Icon icon="logos:docker-icon" width={"100%"} className="logo" />
+      </motion.section>
+    </div>
+  );
+};
+
+export default Skills;
