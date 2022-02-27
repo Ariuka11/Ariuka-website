ff9abbaf928998ebc52c9d49a161d6edf0e46f99
ariuka11 <ariun1118@gmail.com>
2022-02-26 23:36:26 -0500

    Working on Projects section

---
+++ src/components/Projects.tsx
@@ -0,0 +1,12 @@
+const Projects = () => {
+  return (
+    <div className="projectContainer">
+      <section>
+        <h2>Projects</h2>
+      </section>
+      <section>card</section>
+    </div>
+  );
+};
+
+export default Projects;
