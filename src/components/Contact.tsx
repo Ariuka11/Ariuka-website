a35226ebb473d3fea8ef130227b0904f81f442e4
ariuka11 <ariun1118@gmail.com>
2022-02-27 13:34:14 -0500

    fixed nav bar issue and added scroll type

---
+++ src/components/Contact.tsx
@@ -0,0 +1,11 @@
+const Contact = () => {
+  return (
+    <div className="contactContainer">
+      <section>
+        <h2>Contact</h2>
+      </section>
+    </div>
+  );
+};
+
+export default Contact;
