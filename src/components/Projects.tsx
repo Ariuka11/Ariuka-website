1e946af053ca1386f30b2376433f6688c1dc40a5
ariuka11 <ariun1118@gmail.com>
2022-02-27 00:26:49 -0500

    project part is almost done

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,7 +4,84 @@ const Projects = () => {
       <section>
         <h2>Projects</h2>
       </section>
-      <section>card</section>
+      <section className="grid">
+        <div className="card">
+          <img
+            src="https://images.unsplash.com/photo-1645906321292-07a671e6deb6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=700&q=60"
+            alt=""
+          />
+          <div className="title">
+            <h3>Disney Plus Clone</h3>
+            <div className="button">
+              <button>Github</button>
+              <button>Live demo</button>
+            </div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem
+            laboriosam ipsa ullam praesentium dolor labore expedita modi,
+            nostrum magnam iusto facilis sapiente quasi consequatur itaque
+            dolores, odit accusantium, rerum libero.
+          </p>
+        </div>
+        <div className="card">
+          <img
+            src="https://images.unsplash.com/photo-1645906321292-07a671e6deb6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=700&q=60"
+            alt=""
+          />
+          <div className="title">
+            <h3>Disney Plus Clone</h3>
+            <div className="button">
+              <button>Github</button>
+              <button>Live demo</button>
+            </div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem
+            laboriosam ipsa ullam praesentium dolor labore expedita modi,
+            nostrum magnam iusto facilis sapiente quasi consequatur itaque
+            dolores, odit accusantium, rerum libero.
+          </p>
+        </div>
+        <div className="card">
+          <img
+            src="https://images.unsplash.com/photo-1645906321292-07a671e6deb6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=700&q=60"
+            alt=""
+          />
+          <div className="title">
+            <h3>Disney Plus Clone</h3>
+            <div className="button">
+              <button>Github</button>
+              <button>Live demo</button>
+            </div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem
+            laboriosam ipsa ullam praesentium dolor labore expedita modi,
+            nostrum magnam iusto facilis sapiente quasi consequatur itaque
+            dolores, odit accusantium, rerum libero.
+          </p>
+        </div>
+        <div className="card">
+          <img
+            src="https://images.unsplash.com/photo-1645906321292-07a671e6deb6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=700&q=60"
+            alt=""
+          />
+          <div className="title">
+            <h3>Disney Plus Clone</h3>
+            <div className="button">
+              <button>Github</button>
+              <button>Live demo</button>
+            </div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem
+            laboriosam ipsa ullam praesentium dolor labore expedita modi,
+            nostrum magnam iusto facilis sapiente quasi consequatur itaque
+            dolores, odit accusantium, rerum libero.
+          </p>
+        </div>
+      </section>
     </div>
   );
 };
