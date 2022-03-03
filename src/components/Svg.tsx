b39fc129fa882b961294085c51fa8f09a2af87de
ariuka11 <ariun1118@gmail.com>
2022-03-02 23:44:02 -0500

    removed skill animation

--- src/components/Svg.tsx
+++ src/components/Svg.tsx
@@ -35,7 +35,6 @@ const Svg = () => {
   return (
     <motion.section ref={setRefs} className="gridSkill">
       <motion.svg
-        animate={animation1}
         className="logo tall-wide"
         xmlns="http:ß//www.w3.org/2000/svg"
         aria-hidden="true"
@@ -48,7 +47,6 @@ const Svg = () => {
         <path d="M20.809 23.875a2.866 2.866 0 0 0 2.6 1.6c1.09 0 1.787-.545 1.787-1.3c0-.9-.716-1.222-1.916-1.747l-.658-.282c-1.9-.809-3.16-1.822-3.16-3.964c0-1.973 1.5-3.476 3.853-3.476a3.889 3.889 0 0 1 3.742 2.107L25 18.128A1.789 1.789 0 0 0 23.311 17a1.145 1.145 0 0 0-1.259 1.128c0 .789.489 1.109 1.618 1.6l.658.282c2.236.959 3.5 1.936 3.5 4.133c0 2.369-1.861 3.667-4.36 3.667a5.055 5.055 0 0 1-4.795-2.691Zm-9.295.228c.413.733.789 1.353 1.693 1.353c.864 0 1.41-.338 1.41-1.653v-8.947h2.631v8.982c0 2.724-1.6 3.964-3.929 3.964a4.085 4.085 0 0 1-3.947-2.4Z" />
       </motion.svg>
       <motion.svg
-        animate={animation1}
         className="logo tall-wide"
         xmlns="http://www.w3.org/2000/svg"
         aria-hidden="true"
@@ -72,7 +70,6 @@ const Svg = () => {
         />
       </motion.svg>
       <motion.svg
-        animate={animation1}
         custom={0}
         className="logo"
         xmlns="http://www.w3.org/2000/svg"
@@ -102,7 +99,6 @@ const Svg = () => {
         />
       </motion.svg>
       <motion.svg
-        animate={animation1}
         className="logo"
         xmlns="http://www.w3.org/2000/svg"
         aria-hidden="true"
@@ -131,7 +127,6 @@ const Svg = () => {
         />
       </motion.svg>
       <motion.svg
-        animate={animation1}
         className="logo wide"
         xmlns="http://www.w3.org/2000/svg"
         aria-hidden="true"
